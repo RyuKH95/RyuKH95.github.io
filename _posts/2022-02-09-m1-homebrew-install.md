c459ff6b53b50e330301d9849d09ab3a283bc71a
RyuKH95 <gh9503@gmail.com>
2022-02-10 00:28:58 +0900

    init
    init

---
+++ _posts/2022-02-09-m1-homebrew-install.md
@@ -0,0 +1,28 @@
+---
+layout: single
+title: "M1 맥북 homebrew 설치방법"
+categories: Mac
+tag: [M1, Macbook, Air, homebrew, brew]
+toc: true
+---
+
+![homebrew](https://media.vlpt.us/images/franc/post/12d872d8-479e-40d1-ba92-f5324e60a350/brew.png)
+
+## 1. homebrew 파일 다운받기(붙여넣기)
+```terminal
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+```
+
+## 2. homebrew 설치
+1)user_id에 자신의 계정명 넣기
+
+2)아래 두줄 붙여넣기
+```terminal
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/<user_id>/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+```
+
+## 3. homebrew version 확인
+```terminal
+brew —version
+```
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "first",
+        "excerpt":"  ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      },{
+        "title": "[MySQL]콘솔에서 DB접속 방법",
+        "excerpt":"로컬 접속 mysql -u root -p 원격지 접속 mysql -h 원격지주소 -P 포트번호 -u 계정명 -p ex) mysql -h tistory.com -P 1111 -u itgh -p DB접속 use DB명 ex) use itgh 원격지 DB 바로 접속 mysql -h ‘주소’ -P 포트번호 -u 계정명 -p DB명 ex) mysql -h tistory.com -P 1111...","categories": ["Database"],
+        "tags": ["MySQL","MariaDB","DB"],
+        "url": "/database/mysql_ssh/",
+        "teaser": null
+      },{
+        "title": "M1 맥북 homebrew 설치방법",
+        "excerpt":"   1. homebrew 파일 다운받기(붙여넣기)  /bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)   2. homebrew 설치  1)user_id에 자신의 계정명 넣기   2)아래 두줄 붙여넣기  echo 'eval \"$(/opt/homebrew/bin/brew shellenv)\"' &gt;&gt; /Users/&lt;user_id&gt;/.zprofile eval \"$(/opt/homebrew/bin/brew shellenv)\"   3. homebrew version 확인  brew —version  ","categories": ["Mac"],
+        "tags": ["M1","Macbook","Air","homebrew","brew"],
+        "url": "/mac/m1-homebrew-install/",
+        "teaser": null
+      }]
