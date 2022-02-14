0df03748286734d6b75ff6567967a38ad9825144
RyuKH95 <gh9503@gmail.com>
2022-02-14 09:50:24 +0900

    1001 10998

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -24,31 +24,31 @@ var store = [{
         "teaser": null
       },{
         "title": "[백준] 2577",
-        "excerpt":"문제 Hello World!를 출력하시오.   import java.io.*;  class Main {     public static void main(String[] args) {         System.out.println(\"Hello World!\");     } }  ","categories": ["Baekjoon"],
+        "excerpt":"문제  Hello World!를 출력하시오.   코드  import java.io.*;  class Main {     public static void main(String[] args) {         System.out.println(\"Hello World!\");     } }  ","categories": ["Baekjoon"],
         "tags": ["Baekjoon","Hello World!"],
         "url": "/baekjoon/baekjoon-2577/",
         "teaser": null
       },{
         "title": "[백준] 10718",
-        "excerpt":"문제 두 줄에 걸쳐 “강한친구 대한육군”을 한 줄에 한 번씩 출력한다.   import java.io.*;  class Main {     public static void main(String[] args) {         System.out.println(\"강한친구 대한육군\");         System.out.println(\"강한친구 대한육군\");     } }  ","categories": ["Baekjoon"],
+        "excerpt":"문제  두 줄에 걸쳐 “강한친구 대한육군”을 한 줄에 한 번씩 출력한다.   코드  import java.io.*;  class Main {     public static void main(String[] args) {         System.out.println(\"강한친구 대한육군\");         System.out.println(\"강한친구 대한육군\");     } }  ","categories": ["Baekjoon"],
         "tags": ["Baekjoon"],
         "url": "/baekjoon/baekjoon-10718/",
         "teaser": null
       },{
         "title": "[백준] 1000",
-        "excerpt":"문제 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A+B를 출력한다. import java.util.*; public class Main{ public static void main(String args[]){ Scanner sc = new Scanner(System.in); int a, b; a = sc.nextInt(); b...","categories": ["Baekjoon"],
+        "excerpt":"문제 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A+B를 출력한다. 코드 import java.util.*; public class Main{ public static void main(String args[]){ Scanner sc = new Scanner(System.in); int a, b; a = sc.nextInt();...","categories": ["Baekjoon"],
         "tags": ["Baekjoon"],
         "url": "/baekjoon/baekjoon-1000/",
         "teaser": null
       },{
         "title": "[백준] 10171",
-        "excerpt":"문제 아래 예제와 같이 고양이를 출력하시오. \\    /   )  ( ‘) (  /  )  (__)|   import java.io.*;  class Main {     public static void main(String[] args) {         System.out.println(\"\\\\    /\\\\\");         System.out.println(\" )  ( \\')\");         System.out.println(\"(  /  )\");         System.out.println(\" \\\\(__)|\");     } }  ","categories": ["Baekjoon"],
+        "excerpt":"문제  아래 예제와 같이 고양이를 출력하시오.  \\    /\\    )  ( ')   (  /  )    \\(__)|     코드  import java.io.*;  class Main {     public static void main(String[] args) {         System.out.println(\"\\\\    /\\\\\");         System.out.println(\" )  ( \\')\");         System.out.println(\"(  /  )\");         System.out.println(\" \\\\(__)|\");     } }  ","categories": ["Baekjoon"],
         "tags": ["Baekjoon"],
         "url": "/baekjoon/baekjoon-10171/",
         "teaser": null
       },{
         "title": "[백준] 10172",
-        "excerpt":"문제 아래 예제와 같이 개를 출력하시오. |_/| |q p|   /} ( 0 )”””  |”^”`    | ||_/=\\__|   import java.io.*;  class Main {     public static void main(String[] args) {         System.out.println(\"|\\\\_/|\");         System.out.println(\"|q p|   /}\");         System.out.println(\"( 0 )\\\"\\\"\\\"\\\\\");         System.out.println(\"|\\\"^\\\"`    |\");         System.out.println(\"||_/=\\\\\\\\__|\");     } }  ","categories": ["Baekjoon"],
+        "excerpt":"문제  아래 예제와 같이 개를 출력하시오.  |\\_/|   |q p|   /}   ( 0 )\"\"\"\\   |\"^\"`    |   ||_/=\\\\__|     코드  import java.io.*;  class Main {     public static void main(String[] args) {         System.out.println(\"|\\\\_/|\");         System.out.println(\"|q p|   /}\");         System.out.println(\"( 0 )\\\"\\\"\\\"\\\\\");         System.out.println(\"|\\\"^\\\"`    |\");         System.out.println(\"||_/=\\\\\\\\__|\");     } }  ","categories": ["Baekjoon"],
         "tags": ["Baekjoon"],
         "url": "/baekjoon/baekjoon-10172/",
         "teaser": null
