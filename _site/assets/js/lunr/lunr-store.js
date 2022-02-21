790564af55090f87cc9e2994b71f9dffbbdfd87e
RyuKH95 <gh9503@gmail.com>
2022-02-21 10:50:59 +0900

    bj 9498

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -100,4 +100,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-2588/",
         "teaser": null
+      },{
+        "title": "[백준] 1330",
+        "excerpt":"문제 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다. 출력 첫째 줄에 다음 세 가지 중 하나를 출력한다. A가 B보다 큰 경우에는 ‘&gt;’를 출력한다. A가 B보다 작은 경우에는 ‘&lt;’를 출력한다. A와 B가 같은 경우에는...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-1330/",
+        "teaser": null
       }]
