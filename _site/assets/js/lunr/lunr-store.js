2a30249af4b647d1ad88ec61d97ce80c70e39b2b
RyuKH95 <gh9503@gmail.com>
2022-02-22 16:02:50 +0900

    bj 2753

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -106,4 +106,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-1330/",
         "teaser": null
+      },{
+        "title": "[백준] 9498",
+        "excerpt":"문제 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다. 출력 시험 성적을 출력한다. 코드 import java.util.*; public class...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-9498/",
+        "teaser": null
       }]
