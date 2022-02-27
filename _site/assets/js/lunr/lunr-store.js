17682a0db12191f672a28edab326187438d5d25d
RyuKH95 <gh9503@gmail.com>
2022-02-28 00:33:21 +0900

    bj 10950

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -142,4 +142,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-2480/",
         "teaser": null
+      },{
+        "title": "[백준] 2739",
+        "excerpt":"문제 N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다. 입력 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다. 출력 출력형식과 같게 N * 1 부터 N * 9 까지 출력한다. 코드 import java.util.*; public class Main{ public static void main(String args[]){ Scanner...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-2739/",
+        "teaser": null
       }]
