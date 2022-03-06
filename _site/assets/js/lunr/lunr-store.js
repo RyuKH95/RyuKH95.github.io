63e8108e2927bb24aa4f9eaf7c71e56850a94950
RyuKH95 <gh9503@gmail.com>
2022-03-06 13:46:21 +0900

    bj 11022

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -178,4 +178,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-2742/",
         "teaser": null
+      },{
+        "title": "[백준] 11021",
+        "excerpt":"문제 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 각 테스트 케이스마다 “Case #x: “를 출력한 다음, A+B를 출력한다. 테스트 케이스...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-11021/",
+        "teaser": null
       }]
