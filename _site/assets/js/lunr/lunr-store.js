527a7834e7756c83537beb5f69b22cc3b4307405
RyuKH95 <gh9503@gmail.com>
2022-03-08 11:05:55 +0900

    bj 2439

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -190,4 +190,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-11022/",
         "teaser": null
+      },{
+        "title": "[백준] 2438",
+        "excerpt":"문제 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제 입력 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다. 출력 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다. 코드 import java.util.*; public class Main{ public static void main(String args[]){ Scanner sc = new Scanner(System.in); int t;...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-2438/",
+        "teaser": null
       }]
