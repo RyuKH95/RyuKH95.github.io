27972e8370347ac48c140710fd36b487cf653a07
RyuKH95 <gh9503@gmail.com>
2022-03-09 17:36:08 +0900

    10871

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -196,4 +196,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-2438/",
         "teaser": null
+      },{
+        "title": "[백준] 2439",
+        "excerpt":"문제 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오. 입력 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다. 출력 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다. 코드 import java.util.*; public class Main{ public static void main(String args[]){...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-2439/",
+        "teaser": null
       }]
