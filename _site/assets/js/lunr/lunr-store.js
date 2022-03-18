042622b5695f7d45db81ffd9bd966d7f7fc43a9a
RyuKH95 <gh9503@gmail.com>
2022-03-19 00:26:12 +0900

    bj 4344

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -256,4 +256,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-1546/",
         "teaser": null
+      },{
+        "title": "[백준] 8958",
+        "excerpt":"문제 “OOXXOXXOOO”와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다. “OOXXOXXOOO”의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다. OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오. 입력 첫째 줄에 테스트...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-8958/",
+        "teaser": null
       }]
