dd1f226468ae4fd366a68f1850f5bf27db1ba558
RyuKH95 <gh9503@gmail.com>
2022-03-22 00:04:27 +0900

    bj 4673

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -268,4 +268,16 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-4344/",
         "teaser": null
+      },{
+        "title": "[백준] 15596",
+        "excerpt":"문제 정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오. 작성해야 하는 함수는 다음과 같다. Java: long sum(int[] a); (클래스 이름: Test) a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000) 리턴값: a에 포함되어 있는 정수 n개의 합 코드 public...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-15596/",
+        "teaser": null
+      },{
+        "title": "[백준] 4673",
+        "excerpt":"문제 셀프 넘버는 1949년 인도 수학자 D.R. Kaprekar가 이름 붙였다. 양의 정수 n에 대해서 d(n)을 n과 n의 각 자리수를 더하는 함수라고 정의하자. 예를 들어, d(75) = 75+7+5 = 87이다. 양의 정수 n이 주어졌을 때, 이 수를 시작해서 n, d(n), d(d(n)), d(d(d(n))), …과 같은 무한 수열을 만들 수 있다. 예를 들어,...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-4673/",
+        "teaser": null
       }]
