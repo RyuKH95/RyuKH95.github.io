7589c104f2f8db073bcfe736bcd86e2773e1bfe7
RyuKH95 <gh9503@gmail.com>
2022-03-24 09:23:29 +0900

    bj 11720

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -280,4 +280,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-4673/",
         "teaser": null
+      },{
+        "title": "[백준] 11654",
+        "excerpt":"문제 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오. 입력 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다. 출력 입력으로 주어진 글자의 아스키 코드 값을 출력한다. 코드 import java.util.*; public class Main{ public static void main(String args[]){ Scanner sc = new...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-11654/",
+        "teaser": null
       }]
