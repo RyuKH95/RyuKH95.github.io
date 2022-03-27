fea762d8fb52f9a08ccea221901c6f6e2283befc
RyuKH95 <gh9503@gmail.com>
2022-03-27 13:38:59 +0900

    bj 1157

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -298,4 +298,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-10809/",
         "teaser": null
+      },{
+        "title": "[백준] 2675",
+        "excerpt":"문제 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code “alphanumeric” 문자만 들어있다. QR Code “alphanumeric” 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ$%*+-./: 이다. 입력 첫째 줄에 테스트 케이스의 개수...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-2675/",
+        "teaser": null
       }]
