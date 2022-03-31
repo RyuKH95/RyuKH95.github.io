60f17f5d0772324a0d94d014a4ed9912dd70cb1c
RyuKH95 <gh9503@gmail.com>
2022-03-31 15:23:07 +0900

    bj 2941

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -328,4 +328,10 @@ var store = [{
         "tags": ["Baekjoon"],
         "url": "/baekjoon/bj-5622/",
         "teaser": null
+      },{
+        "title": "[백준] 2941",
+        "excerpt":"문제 예전에는 운영체제에서 크로아티아 알파벳을 입력할 수가 없었다. 따라서, 다음과 같이 크로아티아 알파벳을 변경해서 입력했다. 크로아티아 알파벳 변경 č c= ć c- dž dz= đ d- lj lj nj nj š s= ž z= 예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때,...","categories": ["Baekjoon"],
+        "tags": ["Baekjoon"],
+        "url": "/baekjoon/bj-2941/",
+        "teaser": null
       }]
